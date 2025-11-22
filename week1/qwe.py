23709c2f4aa9a1e718b8be1cb333ae59fc3348a5
Данил Лошманов <loshmanov_d@bk.ru>
2025-11-22 19:10:54 +0500

    Поменял классы и сделал мини зоопарк

--- week1/qwe.py
+++ week1/qwe.py
@@ -1,3 +1,17 @@
-class load:
-    def __init__(self):
-        pass
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name)
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def speak(self):
+        print(self.name)
+
+if __name__ == '__main__':
+    dog = Animal('Dog')
+    dog.speak()
